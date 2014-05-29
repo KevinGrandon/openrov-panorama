db3727cafba24a6580fb79058aac34db0a6a17f3
Kevin Grandon <kevingrandon@yahoo.com>
2014-05-29 11:55:24 -0400

    Add initial node.js component

--- panorama/index.js
+++ panorama/index.js
@@ -1,1 +1,62 @@
-console.log('index.');
+(function (exports) {
+
+  var fs = require('fs');
+  var path = require('path');
+
+  function Panorama(name, deps) {
+	if (!(this instanceof Panorama)) {
+		return new Panorama(name, deps);
+	}
+
+	// Whether or not we are currently capturing a panorama
+	this.isCapturing = false;
+
+	this.listen(deps);
+	deps.globalEventLoop.on('photo-added', function (filename) {
+		deps.io.sockets.emit('photo-added', filename);
+		console.log('sending photo to web client');
+	});
+  };
+
+  Panorama.prototype = {
+
+	listen: function(deps) {
+		deps.io.sockets.on('connection', function (socket) {
+			console.log('Panorama:connection');
+			socket.on('panoramastart', this.start.bind(this, deps));
+			socket.on('panoramastop', this.stop.bind(this, deps));
+		}.bind(this));
+	},
+
+	start: function(deps) {
+		console.log('Panorama:start');
+		this.isCapturing = true;
+
+		function takePicture() {
+			deps.rov.camera.snapshot(function (filename) {
+				console.log('Photo taken: ' + filename);
+				deps.io.sockets.emit('photo-added', '/photos/' + path.basename(filename));
+			});
+
+			this.captureTimeout = setTimeout(takePicture.bind(this), 1000);
+		}
+
+		takePicture.call(this);
+	},
+
+	stop: function(deps) {
+		console.log('Panorama:stop');
+		this.isCapturing = false;
+		clearTimeout(this.captureTimeout);
+		this.generate(deps);
+	},
+
+	generate: function(deps) {
+		// Call this after generating to let the UI know that we are ready.
+		dep.io.sockets.emit('photo-stitched', 'some filename');
+	}
+	};
+
+	exports.exports = Panorama;
+
+}(module));
--- panorama/public/js/panorama.js
+++ panorama/public/js/panorama.js
@@ -1,53 +1,58 @@
 (function (exports, $, undefined) {
 
-  function Panorama(cockpit) {
+	function Panorama(cockpit) {
 
-    // Store a reference to the cockpit object
-    this.cockpit = cockpit;
+		// Store a reference to the cockpit object
+		this.cockpit = cockpit;
 
-    // Whether or not we are currently capturing a panorama
-    this.isCapturing = false;
+		// Whether or not we are currently capturing a panorama
+		this.isCapturing = false;
 
-    $('#buttonPanel').append('<button id="panorama-start" class="btn">Start Panorama</button>');
-    $('#buttonPanel').append('<button id="panorama-stop" class="btn">Capture Panorama</button>');
+		$('#buttonPanel').append('<button id="panorama-start" class="btn">Start Panorama</button>');
+		$('#buttonPanel').append('<button id="panorama-stop" class="btn">Capture Panorama</button>');
 
-    this.startButton = $('#panorama-start');
-    this.stopButton = $('#panorama-stop').toggle();
+		this.startButton = $('#panorama-start');
+		this.stopButton = $('#panorama-stop').toggle();
 
-    // Element Listeners
-    this.startButton.click(this.handleStartButton.bind(this));
-    this.stopButton.click(this.handleStopButton.bind(this));
+		// Element Listeners
+		this.startButton.click(this.handleStartButton.bind(this));
+		this.stopButton.click(this.handleStopButton.bind(this));
 
-    // Socket Listeners
-    this.cockpit.socket.on('photo-added', this.handlePhotoAdded.bind(this));
-  }
+		// Socket Listeners
+		this.cockpit.socket.on('photo-added', this.handlePhotoAdded.bind(this));
+		this.cockpit.socket.on('photo-stitched', this.handlePhotoStitched.bind(this));
+	}
 
-  Panorama.prototype = {
+	Panorama.prototype = {
 
-    toggleButtons: function() {
-      this.startButton.toggle();
-      this.stopButton.toggle();
-    },
+		toggleButtons: function() {
+			this.startButton.toggle();
+			this.stopButton.toggle();
+		},
 
-    handleStartButton: function() {
-      this.isCapturing = true;
-      this.cockpit.socket.emit('panoramastart');
-      console.log('send panorama start request to server');
-      this.toggleButtons();
-    },
+		handleStartButton: function() {
+			this.isCapturing = true;
+			this.cockpit.socket.emit('panoramastart');
+			console.log('send panorama start request to server');
+			this.toggleButtons();
+		},
 
-    handleStopButton: function() {
-      this.isCapturing = false;
-      this.cockpit.socket.emit('panoramacapture');
-      console.log('send panorama capture request to server');
-      this.toggleButtons();
-    },
+		handleStopButton: function() {
+			this.isCapturing = false;
+			this.cockpit.socket.emit('panoramacapture');
+			console.log('send panorama capture request to server');
+			this.toggleButtons();
+		},
 
-    handlePhotoAdded: function (filename) {
-      console.log('got new photos', filename);
-    }
-  };
+		handlePhotoAdded: function(filename) {
+			console.log('got new photos', filename);
+		},
 
-  window.Cockpit.plugins.push(Panorama);
+		handlePhotoStitched: function(filename) {
+			console.log('got a stitched photo', filename);
+		}
+	};
+
+	window.Cockpit.plugins.push(Panorama);
 
 }(window, jQuery));
