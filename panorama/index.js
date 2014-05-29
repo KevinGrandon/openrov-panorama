a995d1d4203b843eee9c3e335fa7b50361e4bfd0
Kevin Grandon <kevingrandon@yahoo.com>
2014-05-29 12:32:21 -0400

    Add panoramit interface

--- panorama/index.js
+++ panorama/index.js
@@ -1,60 +1,77 @@
-(function (exports) {
+(function(exports) {
 
-  var fs = require('fs');
-  var path = require('path');
+	var fs = require('fs');
+	var path = require('path');
+	var panoramit = require('panoramit');
 
-  function Panorama(name, deps) {
-	if (!(this instanceof Panorama)) {
-		return new Panorama(name, deps);
-	}
-
-	// Whether or not we are currently capturing a panorama
-	this.isCapturing = false;
-
-	this.listen(deps);
-	deps.globalEventLoop.on('photo-added', function (filename) {
-		deps.io.sockets.emit('photo-added', filename);
-		console.log('sending photo to web client');
-	});
-  };
-
-  Panorama.prototype = {
-
-	listen: function(deps) {
-		deps.io.sockets.on('connection', function (socket) {
-			console.log('Panorama:connection');
-			socket.on('panoramastart', this.start.bind(this, deps));
-			socket.on('panoramastop', this.stop.bind(this, deps));
-		}.bind(this));
-	},
-
-	start: function(deps) {
-		console.log('Panorama:start');
-		this.isCapturing = true;
-
-		function takePicture() {
-			deps.rov.camera.snapshot(function (filename) {
-				console.log('Photo taken: ' + filename);
-				deps.io.sockets.emit('photo-added', '/photos/' + path.basename(filename));
-			});
-
-			this.captureTimeout = setTimeout(takePicture.bind(this), 1000);
+	function Panorama(name, deps) {
+		if (!(this instanceof Panorama)) {
+			return new Panorama(name, deps);
 		}
 
-		takePicture.call(this);
-	},
-
-	stop: function(deps) {
-		console.log('Panorama:stop');
+		// Whether or not we are currently capturing a panorama
 		this.isCapturing = false;
-		clearTimeout(this.captureTimeout);
-		this.generate(deps);
-	},
 
-	generate: function(deps) {
-		// Call this after generating to let the UI know that we are ready.
-		dep.io.sockets.emit('photo-stitched', 'some filename');
+		this.listen(deps);
+		deps.globalEventLoop.on('photo-added', function(filename) {
+			deps.io.sockets.emit('photo-added', filename);
+			console.log('sending photo to web client');
+		});
 	}
+
+	Panorama.prototype = {
+
+		files: [],
+
+		listen: function(deps) {
+			deps.io.sockets.on('connection', function(socket) {
+				console.log('Panorama:connection');
+				socket.on('panoramastart', this.start.bind(this, deps));
+				socket.on('panoramastop', this.stop.bind(this, deps));
+			}.bind(this));
+		},
+
+		start: function(deps) {
+			console.log('Panorama:start');
+			this.isCapturing = true;
+
+			function takePicture() {
+				deps.rov.camera.snapshot(function(filename) {
+					console.log('Photo taken: ' + filename);
+					this.files.push(filename);
+					deps.io.sockets.emit('photo-added', '/photos/' + path.basename(filename));
+				}.bind(this));
+
+				this.captureTimeout = setTimeout(takePicture.bind(this), 5000);
+			}
+
+			takePicture.call(this);
+		},
+
+		stop: function(deps) {
+			console.log('Panorama:stop');
+			this.isCapturing = false;
+			clearTimeout(this.captureTimeout);
+			this.generate(deps);
+		},
+
+		generate: function(deps) {
+
+			panoramit.generate({
+				inputDir: path.join('/photos/'), // TODO
+				inputExt: 'jpg',
+				outputFile: path.join(__dirname, 'out', 'out.jpg'),
+				tempDir: path.join(__dirname, 'tmp'),
+
+				debug: false // optional value in case you want to debug the individual panorama commands
+			}, function(err, outputPath) {
+				// output path is passed through for convenience on success
+			});
+
+			// Call this after generating to let the UI know that we are ready.
+			deps.io.sockets.emit('photo-stitched', 'some filename');
+			this.files = [];
+		}
 	};
 
 	exports.exports = Panorama;
